c3c4a2627bdc0ae92afc750f50c7d04fb64fa3cc
Chrisnatalx <natale.christian.a@gmail.com>
2023-05-13 13:25:45 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { Movies } from './components/Movies'
+import { useMovies } from './hooks/useMovies'
+
+
+
+function App() {
+  const { movies } = useMovies()
+
+  return (
+    <>
+      <div className='page'>
+        <header>
+          <h1>Buscador de pelicula</h1>
+          <form className='form'>
+            <input placeholder='Avengers, Star Wars, The Matrix...' />
+            <button type='submit'>Buscar</button>
+          </form>
+        </header>
+        <main>
+          <Movies movies={movies} />
+        </main>
+
+      </div>
+    </>
+  )
+}
+
+export default App
